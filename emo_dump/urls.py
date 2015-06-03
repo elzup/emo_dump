be31f8cc93a7970eeb5d6c39c0213f4c3f1ebb1d
elzzup <guild0105@gmail.com>
2015-06-04 01:06:37 +0900

    create view

--- emo_dump/urls.py
+++ emo_dump/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from emo_dump import views
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'emo_dump.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^hello/$', views.hello)
 ]
